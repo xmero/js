47bc4924675f70800c9933b6ee6db586de1a21d7
Xavier Meroño <x.merono@gmail.com>
2017-01-18 14:58:14 +0100

    isNumber.js Added

--- isNumber.js
+++ isNumber.js
@@ -1,1 +1,8 @@
-isNumber
+// Define a function called isNumber that receives a value and return true if the value received is a number
+
+function isNumber (n){
+var number = (typeof n === 'number') ? n + ': Is a number' : n + ': Is not a number'
+return number
+}
+
+isNumber()
