624fe3683088356ec8f8f44aba1bd6f10703083e
Xavier Meroño <x.merono@gmail.com>
2017-01-18 16:25:32 +0100

    encodeWord changed to return a String

--- encodeWord.js
+++ encodeWord.js
@@ -1,29 +1,29 @@
-// Define a function called ​encodeWord​ that receives a string and return the codified version of that string replacing the following characters:
+// Define a function called encodeWord that receives a string and return the codified version of that string replacing the following characters:
 
 function encodeWord (s) {
-var decomp = []
-var up = s.toUpperCase()
-decomp = up.split('')
-for (i = 0;i < decomp.length;i++){
-  switch (decomp[i]) {
-    case 'T':
-    decomp[i] = '7'
-    break
-    case 'A':
-    decomp[i] = '4'
-    break
-    case 'S':
-    decomp[i] = '5'
-    break
-    case 'O':
-    decomp[i] = '0'
-    break
-    default:
-    decomp[i] = decomp[i].toLowerCase()
-    break
+  var decomp = []
+  var up = s.toUpperCase()
+  decomp = up.split('')
+  for (var i = 0; i < decomp.length; i++) {
+    switch (decomp[i]) {
+      case 'T':
+        decomp[i] = '7'
+        break
+      case 'A':
+        decomp[i] = '4'
+        break
+      case 'S':
+        decomp[i] = '5'
+        break
+      case 'O':
+        decomp[i] = '0'
+        break
+      default:
+        decomp[i] = decomp[i].toLowerCase()
+        break
+    }
   }
+  return decomp.join('')
 }
-return decomp
-} 
 
 encodeWord()
