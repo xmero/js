009e3d5e9e72d3aace099f0c73f4d10b1b2f9372
Xavier Meroño <x.merono@gmail.com>
2017-01-20 13:52:45 +0100

    translate2.js set for UpperCase

--- translate2.js
+++ translate2.js
@@ -1,12 +1,12 @@
 // Represent a small bilingual lexicon as a Javascript object in the following fashion {"merry":"god", "christmas":"jul", "and":"och", "happy":gott", "new":"nytt", "year":"år"} and use it to translate your Christmas cards from English into Swedish.
 
 var lexicon = {'merry': 'god', 'christmas': 'jul', 'and': 'och', 'happy': 'gott', 'new': 'nytt', 'year': 'år'}
-var christmasCard = 'merry christmas and happy new year'
+var christmasCard = 'Merry Christmas and happy new year'
 var christmasArray = christmasCard.split(' ')
 
 function translate (christmasWord) {
-  if (lexicon[christmasWord]) {
-    return lexicon[christmasWord]
+  if (lexicon[christmasWord.toLowerCase()]) {
+    return lexicon[christmasWord.toLowerCase()]
   }
   return christmasWord
 }
