e8d87743f6f420b9a7194a92b2960f216250878e
Xavier Meroño <x.merono@gmail.com>
2017-01-21 12:01:35 +0100

    charFreq.js Added

--- thirdSet/charFreq.js
+++ thirdSet/charFreq.js
@@ -1,7 +1,12 @@
 // Write a function charFreq() that takes a string and builds a frequency listing of the characters contained in it. Represent the frequency listing as a Javascript object. Try it with something like charFreq("abbabcbdbabdbdbabababcbcbab").
 var charCheck = 'abbabcbdbabdbdbabababcbcbab'
-var charList = []
+
 function charFreq (stringToCheck) {
+  var charList = {}
+  function checkEach (character) {
+    charList[character] = charList[character] + 1 || 1
+  }
+  stringToCheck.split('').forEach(checkEach)
   return charList
 }
 
