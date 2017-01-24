6e25e7b55cff516ec5a3d2db2c0f9ad0cc46f300
Xavier Meroño <x.merono@gmail.com>
2017-01-24 23:39:21 +0100

    challengeV1 Finished

--- challange/spec/challangeSpec.js
+++ challange/spec/challangeSpec.js
@@ -1,21 +1,31 @@
-describe('a function challange that', function () {
+describe('a function printDiamond that', function () {
   it('should exist', function () {
-    expect(challange).toBeDefined()
+    expect(printDiamond).toBeDefined()
   })
   it('should return a string', function () {
-    var result = challange('B')
+    var result = printDiamond('B')
     expect(typeof result).toBe('string')
   })
-  it('should return "A\nB B\n A" when input is B', function () {
-    expect(challange('B')).toBe('A\nB B\n A')
+  it('should return a string even with loweCase input', function () {
+    var result = printDiamond('b')
+    expect(typeof result).toBe('string')
   })
-  it('should return " A\n B B\nC   C\n B B\n A" when input is C', function () {
-    expect(challange('C')).toBe(' A\n B B\nC   C\n B B\n A')
+  it('should return "Error wrong input" when entering NOT a single letter', function () {
+    var result = printDiamond('aa')
+    expect(result).toBe('Error wrong input')
   })
-  it('should return "  A\n  B B\n C   C\nD     D\n C   C\n  B B\n   A" when input is D', function () {
-    expect(challange('D')).toBe('  A\n  B B\n C   C\nD     D\n C   C\n  B B\n   A')
+  it('should return "Error wrong input" when entering a number', function () {
+    var result = printDiamond(5)
+    expect(result).toBe('Error wrong input')
   })
-  it('should return "   A\n   B B\n  C   C\n D     D\nE      E\n  D     D\n  C   C\n   B B\n     A" when input is E', function () {
-    expect(challange('E')).toBe('   A\n   B B\n  C   C\n D     D\nE      E\n  D     D\n  C   C\n   B B\n     A')
+  it('should return "A" when input is A', function () {
+    expect(printDiamond('A')).toBe('A\n')
+  })
+  it('should return " A\nB  B\n A\n" when input is B', function () {
+    expect(printDiamond('B')).toBe(' A\nB  B\n A\n')
+  })
+  it('when input is G string.length should be 151', function () {
+    var result = printDiamond('g')
+    expect(result.length).toEqual(151)
   })
 })
--- challange/src/challange.js
+++ challange/src/challange.js
@@ -1,12 +1,41 @@
-function challange (maxLetter) {
-  if (maxLetter === 'B') {
-    return 'A\nB B\n A'
-  } else if (maxLetter === 'C') {
-    return ' A\n B B\nC   C\n B B\n A'
-  } else if (maxLetter === 'D') {
-    return '  A\n  B B\n C   C\nD     D\n C   C\n  B B\n   A'
-  } else if (maxLetter === 'E') {
-    return '   A\n   B B\n  C   C\n D     D\nE      E\n  D     D\n  C   C\n   B B\n     A'
+
+function printDiamond (letter) {
+  if (typeof letter !== 'string' || letter.length > 1) { return 'Error wrong input' }
+  var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  var n = findPosition(letter) + 1
+  var diamond = ''
+  function findPosition (letter) {
+    for (i = 0; i < alphabet.length; i++) {
+      if (letter.toUpperCase() === alphabet[i]) {
+        return i
+      }
+    }
   }
-  return 'A'
+// Main loop for first half of the diamond
+  for (var i = 0; i < n; i++) {
+    for (var a = 0; a < (n - (i + 1)); a++) {
+      diamond += ' '
+    }
+    diamond += alphabet[i]
+    for (var b = 0; b < (i * 2); b++) {
+      diamond += ' '
+    }
+    if (alphabet[i] === 'A') {
+      diamond += '\n'
+    } else { diamond += (alphabet[i] + '\n') }
+  }
+ // Main loop for the second half of the diamond
+  for (var j = n - 2; j >= 0; j--) {
+    for (var a = 0; a < (n - (j + 1)); a++) {
+      diamond += ' '
+    }
+    diamond += alphabet[j]
+    for (var b = 0; b < (j * 2); b++) {
+      diamond += ' '
+    }
+    if (alphabet[j] === 'A') {
+      diamond += '\n'
+    } else { diamond += (alphabet[j] + '\n') }
+  }
+  return diamond
 }
