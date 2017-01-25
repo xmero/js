0cc41c23357d3a736f0c3a28ee80c21ff089c798
Xavier Meroño <x.merono@gmail.com>
2017-01-25 09:16:32 +0100

    challange updated

--- challange/spec/challangeSpec.js
+++ challange/spec/challangeSpec.js
@@ -1,31 +1,37 @@
-describe('a function printDiamond that', function () {
+describe('a function mysteryChallange that', function () {
   it('should exist', function () {
-    expect(printDiamond).toBeDefined()
+    expect(mysteryChallange).toBeDefined()
   })
   it('should return a string', function () {
-    var result = printDiamond('B')
+    var result = mysteryChallange('B')
     expect(typeof result).toBe('string')
   })
   it('should return a string even with loweCase input', function () {
-    var result = printDiamond('b')
+    var result = mysteryChallange('b')
     expect(typeof result).toBe('string')
   })
   it('should return "Error wrong input" when entering NOT a single letter', function () {
-    var result = printDiamond('aa')
+    var result = mysteryChallange('aa')
     expect(result).toBe('Error wrong input')
   })
   it('should return "Error wrong input" when entering a number', function () {
-    var result = printDiamond(5)
+    var result = mysteryChallange(5)
     expect(result).toBe('Error wrong input')
   })
   it('should return "A" when input is A', function () {
-    expect(printDiamond('A')).toBe('A\n')
+    expect(mysteryChallange('A')).toBe('A\n')
   })
   it('should return " A\nB  B\n A\n" when input is B', function () {
-    expect(printDiamond('B')).toBe(' A\nB  B\n A\n')
+    expect(mysteryChallange('B')).toBe(' A\nB  B\n A\n')
+  })
+  it('should return "  A\n B  B\nC    C\n B  B\n  A\n" when input is C', function () {
+    expect(mysteryChallange('C')).toBe('  A\n B  B\nC    C\n B  B\n  A\n')
+  })
+  it('should return "   A\n  B  B\n C    C\nD      D\n C    C\n  B  B\n   A\n" when input is D', function () {
+    expect(mysteryChallange('D')).toBe('   A\n  B  B\n C    C\nD      D\n C    C\n  B  B\n   A\n')
   })
   it('when input is G string.length should be 151', function () {
-    var result = printDiamond('g')
+    var result = mysteryChallange('G')
     expect(result.length).toEqual(151)
   })
 })
--- challange/src/challange.js
+++ challange/src/challange.js
@@ -1,5 +1,5 @@
 
-function printDiamond (letter) {
+function mysteryChallange (letter) {
   if (typeof letter !== 'string' || letter.length > 1) { return 'Error wrong input' }
   var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
   var n = findPosition(letter) + 1
